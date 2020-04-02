afdd06e538fbfecb3b59a37f60420cb723f29331
camanjar <camanjar@asu.edu>
2020-04-02 05:01:25 -0700

    Ascii illustrations for villains

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,26 @@ package main.java;
 
 import main.java.Villains.*;
 
+/**
+ * TODO: Finish creating villains and layer using
+ *      builder and factory (layer/characters)
+ */
 
 public class Main {
 
     public static void main(String[] args) {
 
-        VillainsFactory villainsFactory = new VillainsFactory(Villains.BOWSER);
+        VillainsFactory villainsFactory = new VillainsFactory(Villains.EVIL_MUSHROOM);
         villainsFactory.catchPhrase();
+
+        VillainsFactory villainsFactory2 = new VillainsFactory(Villains.STARMAN);
+        villainsFactory2.catchPhrase();
+
+        VillainsFactory villainsFactory3 = new VillainsFactory(Villains.MOBILE_CACTUS);
+        villainsFactory3.catchPhrase();
+
     }
+
+
 }
+
